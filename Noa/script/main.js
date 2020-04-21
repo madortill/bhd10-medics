aff63f5f258a6d274b9a2674b23c33bb16a607df
u75645 <u75645@lotem.il>
2020-04-21 18:01:54 +0300

    make hover elements close when when clicking screen(body)

--- Noa/script/main.js
+++ Noa/script/main.js
@@ -274,12 +274,22 @@ function onScroll(event) {
     progBar.style.width = (document.documentElement.scrollTop / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) * 100 + "vw";
 }
 
-// add listeners for slide out
+// add listeners for .slide-out & .link
 function creatHoverListeners() {
     let hover = document.querySelectorAll(".slide-out, .link");
     for (let i = 0; i < hover.length; i++) {
         const element = hover[i];
         element.addEventListener("click", openHover);
+        element.classList.remove("open");
+    }
+}
+
+// remove listeners for slide out
+function removeHoverListeners() {
+    let hover = document.querySelectorAll(".slide-out, .link");
+    for (let i = 0; i < hover.length; i++) {
+        const element = hover[i];
+        element.removeEventListener("click", openHover);
     }
 }
 
@@ -289,14 +299,16 @@ function creatHoverListeners() {
  * @param {Event} event 
  */
 function openHover(event) {
-    let element=event.currentTarget;
-    // if(event.currentTarget.classList.indexOf("tab") != -1){
-    //     element = element.parentElement;
-    // }
-    element.classList.add("open");
-    element.removeEventListener("click", openHover);
+    removeHoverListeners();
+    // let element=event.currentTarget;
+    // // if(event.currentTarget.classList.indexOf("tab") != -1){
+    // //     element = element.parentElement;
+    // // }
+    event.currentTarget.classList.add("open");
+    // element.removeEventListener("click", openHover);
     setTimeout(() => {
-        element.addEventListener("click", closeHover);
+        document.body.addEventListener("click", closeHover);
+        // element.addEventListener("click", closeHover);
         // element.parentElement.addEventListener("click", closeSlideOut);
     }, 100);
 }
@@ -307,10 +319,12 @@ function openHover(event) {
  * @param {Event} event 
  */
 function closeHover(event) {
-    let element = event.currentTarget;
-    element.classList.remove("open");
-    // element.querySelector(".tab").addEventListener("click", openSlideOut);
-    element.addEventListener("click", openHover);
-    element.removeEventListener("click", closeHover);
+    document.body.removeEventListener("click",closeHover);
+    creatHoverListeners();
+    // let element = event.currentTarget;
+    // element.classList.remove("open");
+    // // element.querySelector(".tab").addEventListener("click", openSlideOut);
+    // element.addEventListener("click", openHover);
+    // element.removeEventListener("click", closeHover);
 }
 
