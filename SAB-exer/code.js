cd1cc115dd4297ebb4d3c7119057a4b96c565c57
u75645 <u75645@lotem.il>
2020-04-26 15:51:42 +0300

    added functions and changed measurements

--- SAB-exer/code.js
+++ SAB-exer/code.js
@@ -8,6 +8,8 @@ var bWasWrong;
 var nNumPicks = 0;
 
 $(function(){
+    $(".ok").on("touchend", removeTurn);
+    $(".navigate").on("touchend", Home);
     for(var i=1; i<=3; i++) {
         $("#exemination" + i).on("touchend", pressStage);
     }
@@ -15,57 +17,27 @@ $(function(){
     $(".about").on("touchend", about);
 });
 
-/**
- * on click burger sign :
- * opens nav 
- * adds listeners to icons
- * @param {Event} event 
- */
-function onClickBurger(event) {
-    burgerNav.classList.add("open");
-    burgerNav.querySelector(".x-icon").addEventListener("touchend", closeBurgerNav);
-    let icons = burgerNav.querySelectorAll(".icon");
-    for (let i = 0; i < icons.length; i++) {
-        const element = icons[i];
-        element.addEventListener("touchend", onClickIcon);
-    }
-}
-
-/**
- * on click icon in burger nav : 
- * change src to marked image 
- * un mark prev image
- * @param {Event} event 
- */
-function onClickIcon(event) {
-    let icons = burgerNav.querySelectorAll(".icon");
-    for (let i = 0; i < icons.length; i++) {
-        const element = icons[i];
-        element.querySelector("img").src = element.querySelector("img").src.replace("-current", "");
-    }
-
-    event.currentTarget.querySelector("img").src = event.currentTarget.querySelector("img").src.replace(".svg", "-current.svg");
-}
-
-/**
- * on click x icon in burger nav : 
- * close burger nav
- * remove listeners from icons
- * @param {Event} event 
- */
-function closeBurgerNav(event) {
-    burgerNav.classList.remove("open");
-    burgerNav.querySelector(".x-icon").removeEventListener("touchend", closeBurgerNav);
-    let icons = burgerNav.querySelectorAll(".icon");
-    for (let i = 0; i < icons.length; i++) {
-        const element = icons[i];
-        element.removeEventListener("touchend", onClickIcon);
-    }
+function removeTurn(event) {
+    $(".turn-div").hide();
 }
 
 function about() {
-    
-}    
+    if(!bPressedAbout) {
+        bPressedAbout = true;
+        $(".about-div").show();
+        $(".head").html("קינמטיקה - אודות");
+        $(".about").attr("src", "assets/images/about/play.svg");
+    } else {
+        bPressedAbout = false;
+        $(".about-div").hide();
+        $(".head").html("קינמטיקה");
+        $(".about").attr("src", "assets/images/about.svg");
+    }
+}  
+
+function Home() {
+    window.location.href = "https://mador-till-prod.github.io/BHD-10-Medics/big-site/";
+}
 
 function checkStep(event) {
     if(Number(this.id.charAt(4)) === nStepCounter) {
@@ -75,11 +47,11 @@ function checkStep(event) {
         if(nStepCounter < 4) {
             if(nStepCounter === 1) {
                 $(".steps").show();
-                $(".instructions").css({height: '45vh'});
-                $(".instructions").css({marginTop: '-15vh'});
-                $(".instructions").css({paddingTop: '15vh'});
-                $(".instructions").css({paddingBottom: '10vh'});
-                $(".instructions").css({width: '60vh'});
+                $(".instructions").css({height: '45%'});
+                $(".instructions").css({marginTop: '-15%'});
+                $(".instructions").css({paddingTop: '15%'});
+                $(".instructions").css({paddingBottom: '10%'});
+                $(".instructions").css({width: '60%'});
                 $(".instructions").css({backgroundImage: 'url("assets/images/note7.svg")'});
                 $(".instructions-text").html("");
             }
@@ -99,7 +71,7 @@ function checkStep(event) {
                 if($(".input").val() !== "") {
                     $(".correct").html("הדיווח הראשוני יכלול את הפרטים הבאים:");
                     $(".correct").after("<p>מיקום, מתאר, מספר ומצב הפצועים, מי מדווח.</p>");
-                    $(".correct").css({marginRight: "2vw"});
+                    $(".correct").css({marginRight: "2%"});
                     $(".what").hide();
                     $(".check1").hide();
                     $(".input").hide();
@@ -134,11 +106,11 @@ function nextStage(event) {
     $(".next").hide();
     $(".nextstep").hide();
     $(".instructions").show();
-    $(".instructions").css({height: '60vh'});
-    $(".instructions").css({marginTop: '1vh'});
-    $(".instructions").css({paddingTop: '6vh'});
+    $(".instructions").css({height: '60%'});
+    $(".instructions").css({marginTop: '1%'});
+    $(".instructions").css({paddingTop: '6%'});
     $(".instructions").css({paddingBottom: '0'});
-    $(".instructions").css({width: '24vw'});
+    $(".instructions").css({width: '24%'});
     $(".instructions").css({backgroundImage: 'url("assets/images/note1.svg")'});
     for(var i = 1; i<=3; i++) {
         $("#exemination" + i).show();
@@ -148,7 +120,7 @@ function nextStage(event) {
         $("#exemination" + i).css({opacity: "0.7"});
     }
     $(".instructions-text").html("מה השלב הבא בבדיקה?");
-    $(".instructions-text").css({marginRight: "2vw", width: "20vw"});
+    $(".instructions-text").css({marginRight: "2%", width: "20%"});
     $(".options").hide();
 }
 
@@ -164,15 +136,15 @@ function pressStage(event) {
         $("#exemination" + nCurrStage).off("touchend", pressStage);
         if (nCurrStage === 1) {
             $(".circle").show();
-            $(".instructions").css({marginTop: '-5vh'});
+            $(".instructions").css({marginTop: '-5%'});
             $(".instructions-text").html("נכון מאוד! קודם כל יש לדאוג לבטיחות, על מנת למנוע היווצרות פצועים נוספים.");
-            $(".instructions-text").css({marginRight: "1vw"});
+            $(".instructions-text").css({marginRight: "1%"});
             $(".next").show();
             $(".next").on("touchend", function() {
                 $(".instructions-text").html("מה סדר הפעולות בשלב הS? לחצו על הפריטים לפי הסדר.");
-                $(".instructions-text").css({marginRight: "2vw"});
+                $(".instructions-text").css({marginRight: "2%"});
                 $(".instructions").css({backgroundImage: 'url("assets/images/note2.svg")'});
-                $(".instructions").css({height: '30vh'});
+                $(".instructions").css({height: '30%'});
                 for(var i=1; i<=4; i++) {
                     $("#clue" + i).show();
                     $("#clue" + i).on("touchend", checkStep);
@@ -183,7 +155,7 @@ function pressStage(event) {
             nStepCounter = 1;
             $(".circle").attr("src", "assets/images/a.svg");
             $(".instructions-text").html("נכון מאוד! עכשיו יש לפתוח את נתיב האויר של המטופל. בחר את הצעדים בשלב הA לפי הסדר:");
-            $(".instructions-text").css({marginRight: "1vw"});
+            $(".instructions-text").css({marginRight: "1%"});
             $(".stage2").show();
             for(var i=1; i<=7; i++) {
                 $("#airway" + i).on("touchend", checkAirway);
@@ -192,15 +164,15 @@ function pressStage(event) {
             $(".circle").attr("src", "assets/images/b.svg");
             $(".options2").css({display: "flex"});
             $(".instructions-text").html("איך נתחיל את שלב הB?");
-            $(".instructions-text").css({marginRight:"6vw", width: "30vw"});
-            $(".instructions").css({backgroundImage: 'url("assets/images/note7.svg")', width: '60vh', marginTop: '-15vh', height: "75vh", paddingLeft: "5vw"});
+            $(".instructions-text").css({marginRight:"6%", width: "30%"});
+            $(".instructions").css({backgroundImage: 'url("assets/images/note7.svg")', width: '60%', marginTop: '-15%', height: "75%", paddingLeft: "5%"});
             $(".radio").css({margin: "0"});
             nPicked = 0;
             for(var i=3; i<=6; i++) {
                 $("#radio" + i).on("touchend", pickAnswer);
             }
             $(".check2").show();
-            // $(".check2").css({marginBottom: "-2vw"});
+            // $(".check2").css({marginBottom: "-2%"});
             $(".check2").on("touchend", checkRadio2);
         }
     } else {
@@ -226,15 +198,15 @@ function checkAirway(event) {
         nStepCounter++;
         if(nStepCounter > 6) {
             $(".instructions").css({backgroundImage: 'url("assets/images/note6.svg")'});
-            $(".instructions").css({width: "60vw"});
-            $(".instructions").css({marginTop: "-5vh"});
+            $(".instructions").css({width: "60%"});
+            $(".instructions").css({marginTop: "-5%"});
             for(var i=1; i<=7; i++) {
                 $("#airway" + i).hide();
             }
             $(".conscious-cont").css({display: "flex"});
             $(".divs").css({display: "flex"});
             $(".instructions-text").html("כל הכבוד! מהן רמות ההכרה שיכולות להיות לפצוע?");
-            $(".instructions-text").css({marginRight: "4vw"});
+            $(".instructions-text").css({marginRight: "4%"});
             $(".conscious").on("touchmove", drag);
             window.dragged = $(".dragover");
             $(".conscious").on("touchend", dropItem);
@@ -312,15 +284,15 @@ function checkDrag(event) {
             $("#div" + i).css({color: "rgb(28, 163, 109)"});
         }
         $(".next").show();
-        $(".next").css({marginRight: "55vw", marginTop: "48vh"})
+        $(".next").css({marginRight: "55%", marginTop: "48%"})
         $(".next").on("touchend", function(){
-            $(".instructions").css({height: '45vh',marginTop: '-15vh',paddingTop: '5vh',paddingBottom: '30vh',width: '60vh',backgroundImage: 'url("assets/images/note7.svg")'});
+            $(".instructions").css({height: '45%',marginTop: '-15%',paddingTop: '5%',paddingBottom: '30%',width: '60%',backgroundImage: 'url("assets/images/note7.svg")'});
             $(".instructions").append('<div class="options"><div class="radios"><img class="radio" id="radio1" src="assets/images/option.svg" /><img class="radio" id="radio2" src="assets/images/option.svg" /></div><div class="answer"><p>Jaw Thrust - דחיקת לסת</p><p>Head Tilt - הטיית ראש</p></div></div>');
             $(".instructions-text").html("פצוע נפגע כתוצאה מפיצוץ, באיזו שיטה נשתמש על מנת לפתוח נתיב אוויר?");
-            $(".instructions-text").css({width: "30vw", marginRight: "4.5vw"});
+            $(".instructions-text").css({width: "30%", marginRight: "4.5%"});
             $(".conscious").hide();
             $(".div").hide();
-            $(".check2").css({position: "absolute", right: "2vw", bottom: "-41vw"});
+            $(".check2").css({position: "absolute", right: "2%", bottom: "-41%"});
             $(".check2").off("touchend", checkDrag);
             for(var i=1; i<=4; i++) {
                 $("#clue" + i).hide();
@@ -360,11 +332,11 @@ function checkRadio(event) {
         nNumPicks = 0;
         bWasWrong = false;
         bWasPicked = false;
-        $(".instructions").css({height: '35vh',paddingTop: '15vh'});
+        $(".instructions").css({height: '35%',paddingTop: '15%'});
         $(".instructions-text").html('נכון מאוד! נשתמש בדחיקת לסת כשיש חשש לפגיעה בעמש"צ או כשלא ידוע לנו סיפור המקרה.');
-        $(".instructions-text").css({marginRight: "3vw"});
+        $(".instructions-text").css({marginRight: "3%"});
         $(".next").show();
-        $(".next").css({marginRight: "25vw"});
+        $(".next").css({marginRight: "25%"});
         $(".check2").hide();
         $(".check2").off("touchend", checkRadio);
         $(".options").hide();
@@ -376,7 +348,7 @@ function checkRadio(event) {
         }
     } else {
         $(".wrong").show();
-        $(".wrong").css({right: "35vw"});
+        $(".wrong").css({right: "35%"});
         if(!bWasWrong) {
             nMistakes++;
         }
@@ -395,16 +367,16 @@ function checkRadio2(event) {
         $(".options2").css({display: 'none'});
         $(".check2").hide();
         $(".instructions-text").html("נכון מאוד! קודם כל יש לסרוק את בית החזה בהסתכלות ובמישוש. אין לאטום חורי ירי בשום אופן!");
-        $(".instructions-text").css({marginRight: '4vw', marginTop: "7vh"});
+        $(".instructions-text").css({marginRight: '4%', marginTop: "7%"});
         $(".next").show();
-        $(".next").css({marginTop: "60vh", marginRight: "30vw"});
+        $(".next").css({marginTop: "60%", marginRight: "30%"});
         $(".next").off("touchend", nextStage);
         $(".next").on("touchend", function(){
             $(".next").hide();
             $(".scan").hide();
             $(".instructions-text").html("מדדתם למטופל 12 נשימות ב-30 שניות. האם התוצאה מעידה על נשימה תקינה?");
-            $(".instructions-text").css({marginRight: '3vw', marginTop: "8vh"});
-            $(".instructions").css({backgroundImage: 'url("assets/images/note1.svg")', height: '60vh', width: '34vw',  marginTop: '-10vh', marginRight: '0.5vw', paddinTop: '6vh', paddingRight: '3vw', paddingLeft: '4vw'});
+            $(".instructions-text").css({marginRight: '3%', marginTop: "8%"});
+            $(".instructions").css({backgroundImage: 'url("assets/images/note1.svg")', height: '60%', width: '34%',  marginTop: '-10%', marginRight: '0.5%', paddinTop: '6%', paddingRight: '3%', paddingLeft: '4%'});
             $(".options").hide();
             $(".yes-or-no").show();
             $("#yes").on("touchend", checkYesOrNo);
@@ -412,7 +384,7 @@ function checkRadio2(event) {
         });
     } else {
         $(".wrong").show();
-        $(".wrong").css({right: '35vw'});
+        $(".wrong").css({right: '35%'});
         if(!bWasWrong) {
             nMistakes++;
         }
@@ -438,11 +410,11 @@ function checkYesOrNo(event) {
             bWasWrong = true;
         }
     }
-    $(".instructions-text").css({marginTop: "3vh", marginRight: "5vw"})
+    $(".instructions-text").css({marginTop: "3%", marginRight: "5%"})
     $(".instructions").append('<div class="absolute breath"<p>בדיקת הנשימה תתבצע למשך 30 שניות ונכפיל את התוצאה פי 2.</p><p>נשימה תקינה היא 8-20 נשימות בדקה.</p><p>2X12=24</p><p>כלומר לא בטווח התקין.</p></div>');
-    $(".instructions").css({height: "80vh", width:"45vw",   marginTop: '-15vh'});
+    $(".instructions").css({height: "80%", width:"45%",   marginTop: '-15%'});
     $(".next").show();
-    $(".next").css({position: "absolute", bottom: '5vw', right: '20vw'});
+    $(".next").css({position: "absolute", bottom: '5%', right: '20%'});
     $(".yes-or-no").hide();
     $("#yes").hide();
     $("#no").hide();
